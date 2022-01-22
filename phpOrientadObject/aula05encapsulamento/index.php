dba69d59a1386183a7d9163384bde0b6235ce5ea
randerteixeira <67031015+randerteixeira@users.noreply.github.com>
2022-01-22 18:17:28 -0300

    aula encapsulamento

---
+++ phpOrientadObject/aula05encapsulamento/index.php
@@ -0,0 +1,30 @@
+<?php
+
+
+class Post
+{
+    public int $id;
+    public int $like = 0;
+    public array $coment = [];
+    public string $autor;
+
+    public function aumentarLikes()
+    {
+        $this->like++;
+    }
+    public function setAuthor($n)
+    {
+        $this->autor = ucfirst($n);
+    }
+    public function getAuthor()
+    {
+        return $this->autor;
+    }
+}
+$post1=new Post();
+$post1->setAuthor('rander gabriel Teixeira');
+
+$post2=new Post();
+$post2->setAuthor('mariana');
+echo "post1".$post1->like ."<br>  Autor:".$post1->autor;
+echo "post1".$post2->like ."<br> Autor:".$post2->autor;
